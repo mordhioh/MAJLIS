316ba51606432527fed5c469341a1d39ec0e8dd2
mar <a195074@siswa.ukm.edu.my>
2026-03-29 16:11:48 +0800

    update: updating config.js

--- src/config.js
+++ src/config.js
@@ -1,16 +1,16 @@
 export const eventDetails = {
   title: 'Majlis Kesyukuran dan Doa Selamat',
   subtitle: 'بسم الله الرحمن الرحيم',
-  invitation: `Dengan penuh kasih dan sedikit paksaan 😆, kami sekeluarga menjemput pakcik, makcik, abang, kakak, adik-adik dan anak-anak untuk hadir ke Majlis Kesyukuran dan Doa Selamat kami 🤍
+  invitation: `Dengan penuh kasih, kami sekeluarga menjemput pakcik, makcik, abang, kakak, adik-adik dan anak-anak untuk hadir ke Majlis Kesyukuran dan Doa Selamat sempena keberangkatan kami ke Tanah Suci Makkah Al-Mukarramah untuk menunaikan Fardhu Haji 🤍
 
 Semoga dengan kehadiran kita bersama, majlis ini diberkati dan dipermudahkan 🤍`,
   tarikh: 'Sabtu, 04 April 2026',
-  masa: '4:00 petang - selesai',
+  masa: '4:00 petang - Selesai',
   lokasi: 'Pondok Turath Amanullah Rawang',
   googleMapsUrl: 'https://www.google.com/maps/dir//Pondok+Turath+Amanullah+Rawang,+8HQR%2BRR,+48200+Rawang,+Selangor/@3.3557144,101.5361134,12z',
   aturcara: [
     { masa: '4:00', acara: 'Ketibaan tetamu dan Solat Asar' },
-    { masa: '4:30', acara: 'Bacaan Yassin & Doa Selamat' },
+    { masa: '4:30', acara: 'Bacaan Tahlil & Doa Selamat' },
     { masa: '5:00', acara: 'Jamuan Makan' },
   ],
 }
